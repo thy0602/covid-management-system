4567262ea1c343eccab90bd15cb13f74b80fb8f4
Nguyễn Tấn Thìn <ngtanthin21@gmail.com>
2022-01-05 15:19:07 +0700

    Update sql script

--- postgresql/covid_management_script.sql
+++ postgresql/covid_management_script.sql
@@ -41,9 +41,11 @@ CREATE TABLE "user" (
 	"name" varchar(50) NOT NULL,
 	"year_of_birth" int NOT NULL,
 	"address" varchar NOT NULL,
-	"max_basket" int NOT NULL,
+	"max_baskets" int NOT NULL,
 	"basket_timelimit" int NOT NULL,
 	"username" varchar(50) NOT NULL,
+	"current_status" varchar(10),
+	"current_location" varchar(100),
 	
  	FOREIGN KEY (username) REFERENCES account(username)
 );
@@ -181,28 +183,27 @@ CREATE TABLE "order_product" (
 -- -----------------------------
 -- Table account
 -- -----------------------------
-BEGIN;
-INSERT INTO "account" VALUES ('admin', '$2b$10$VDqC/tAM2BZ/GIX2CuDtPOTptGgEZjZ9YI.IL2igb0qOK0VyWNHUS', 'admin', false);
-
-INSERT INTO "account" VALUES ('manager', '$2b$10$ygjxelzrzamQxVDTMC8zyOmTS01JtlkOSD4SZjYgmW2tx.EML3jAq', 'manager', false);
-
-INSERT INTO "account" VALUES ('ID_001', '$2b$10$UU/.rQwJRux4HLMqDue37OeY1S.BByJb7l3kI.noOeQ.PLu3v.DK6', 'user', false);
-INSERT INTO "account" VALUES ('ID_002', '$2b$10$XXFLhavUekaG8AgYlPMA..ZuLZ3rmx/15lNC/oDiCTXLAZMXfct4m', 'user', false);
-INSERT INTO "account" VALUES ('ID_003', '$2b$10$xHVqAckOGbDP8kuOtSt3Pu7iHO7xDSKRWWfIyhsDT2M4YuOXXcWGm', 'user', false);
-INSERT INTO "account" VALUES ('ID_004', '$2b$10$Ct3W5S5H.9uUk2SBava.yOBYWQLRb0eg4K2gedKPfOMV3C6h96htq', 'user', false);
-INSERT INTO "account" VALUES ('ID_005', '$2b$10$QigU8ZBUC5aKi318boewyuLabgQYdIo.k6I2FHTsgkQo8zFQJVeFK', 'user', false);
-COMMIT;
+INSERT INTO "account"("username", "password", "role", "is_deleted")
+VALUES
+	('admin', '$2b$10$VDqC/tAM2BZ/GIX2CuDtPOTptGgEZjZ9YI.IL2igb0qOK0VyWNHUS', 'admin', false),
+	('manager', '$2b$10$ygjxelzrzamQxVDTMC8zyOmTS01JtlkOSD4SZjYgmW2tx.EML3jAq', 'manager', false),
+	('ID_001', '$2b$10$UU/.rQwJRux4HLMqDue37OeY1S.BByJb7l3kI.noOeQ.PLu3v.DK6', 'user', false),
+	('ID_002', '$2b$10$XXFLhavUekaG8AgYlPMA..ZuLZ3rmx/15lNC/oDiCTXLAZMXfct4m', 'user', false),
+	('ID_003', '$2b$10$xHVqAckOGbDP8kuOtSt3Pu7iHO7xDSKRWWfIyhsDT2M4YuOXXcWGm', 'user', false),
+	('ID_004', '$2b$10$Ct3W5S5H.9uUk2SBava.yOBYWQLRb0eg4K2gedKPfOMV3C6h96htq', 'user', false),
+	('ID_005', '$2b$10$QigU8ZBUC5aKi318boewyuLabgQYdIo.k6I2FHTsgkQo8zFQJVeFK', 'user', false);
 
 -- -----------------------------
 -- Table user
 -- -----------------------------
-BEGIN;
-INSERT INTO "user" VALUES (DEFAULT, 'thin', 2000, 'Vietnam', 5, 1, 'ID_001');
-INSERT INTO "user" VALUES (DEFAULT, 'duy', 1999, 'Vietnam', 5, 1, 'ID_002');
-INSERT INTO "user" VALUES (DEFAULT, 'thy', 2001, 'Vietnam', 5, 1, 'ID_003');
-INSERT INTO "user" VALUES (DEFAULT, 'nhan', 1998, 'Vietnam', 5, 1, 'ID_004');
-INSERT INTO "user" VALUES (DEFAULT, 'trung', 2002, 'Vietnam', 5, 1, 'ID_005');
-COMMIT;
+INSERT INTO "user"("id", "name", "year_of_birth", "address", "max_baskets", "basket_timelimit", "username")
+VALUES 
+	(DEFAULT, 'thin', 2000, 'Vietnam', 5, 1, 'ID_001'),
+	(DEFAULT, 'thin', 2000, 'Vietnam', 5, 1, 'ID_001'),
+	(DEFAULT, 'duy', 1999, 'Vietnam', 5, 1, 'ID_002'),
+	(DEFAULT, 'thy', 2001, 'Vietnam', 5, 1, 'ID_003'),
+	(DEFAULT, 'nhan', 1998, 'Vietnam', 5, 1, 'ID_004'),
+	(DEFAULT, 'trung', 2002, 'Vietnam', 5, 1, 'ID_005');
 
 -- -----------------------------
 -- Table relate
@@ -223,25 +224,88 @@ COMMIT;
 -- -----------------------------
 -- Table product
 -- -----------------------------
--- INSERT "product" INTO VALUES (DEFAULT, 'Khẩu trang', 'hộp', 30000);
--- INSERT "product" INTO VALUES (DEFAULT, 'Nước rửa tay', 'chai', 40000);
--- INSERT "product" INTO VALUES (DEFAULT, 'Kem đánh răng', 'tuýp', 35000);
--- INSERT "product" INTO VALUES (DEFAULT, 'Bàn chải đánh răng', 'cái', 38000);
--- INSERT "product" INTO VALUES (DEFAULT, 'Khăn giấy', 'hộp', 20000);
+BEGIN;
+INSERT INTO "product"("id", "name", "unit", "price")
+VALUES 
+    (DEFAULT, 'Bắp cải', 'gam', 500),
+	(DEFAULT, 'Xà lách', 'gam', 500),
+	(DEFAULT, 'Khoai tây', 'gam', 500),
+	(DEFAULT, 'Cà rốt', 'gam', 500),
+	(DEFAULT, 'Cam', 'gam', 500),
+	(DEFAULT, 'Táo', 'gam', 500),
+ROLLBACK;
+
+INSERT INTO "product"("id", "name", "unit", "price")
+VALUES 
+	(DEFAULT, 'Thịt heo', 'gam', 1000),
+	(DEFAULT, 'Thịt bò', 'gam', 1000),
+	(DEFAULT, 'Thịt gà', 'gam', 1000),
+	(DEFAULT, 'Trứng gà', 'gam', 1000),
+	(DEFAULT, 'Cá', 'gam', 1000),
+	(DEFAULT, 'Tôm', 'gam', 1000),
+ROLLBACK;
+
+INSERT INTO "product"("id", "name", "unit", "price")
+VALUES 
+	(DEFAULT, 'Cơm cháy', 'gói', 30000),
+	(DEFAULT, 'Rong biển', 'gói', 25000),
+	(DEFAULT, 'Mì gói', 'gói', 7000),
+	(DEFAULT, 'Xúc xích', 'gói', 32000),
+ROLLBACK;
+
+INSERT INTO "product"("id", "name", "unit", "price")
+VALUES 
+	(DEFAULT, 'Nước khoáng', 'chai', 6000),
+	(DEFAULT, 'Nước trái cây', 'chai', 12000),
+	(DEFAULT, 'Mật ong', 'chai', 50000),
+ROLLBACK;
+
+INSERT INTO "product"("id", "name", "unit", "price")
+VALUES 
+	(DEFAULT, 'Sữa tươi', 'hộp', 10000),
+	(DEFAULT, 'Sữa chua', 'hộp', 7000),
+	(DEFAULT, 'Sữa lúa mạch', 'hộp', 12000),
+	(DEFAULT, 'Sữa hạt', 'hộp', 20000),
+ROLLBACK;
+
+INSERT INTO "product"("id", "name", "unit", "price")
+VALUES 
+    (DEFAULT, 'Khẩu trang', 'hộp', 30000),
+	(DEFAULT, 'Nước rửa tay', 'chai', 40000),
+    (DEFAULT, 'Kem đánh răng', 'tuýp', 35000),
+    (DEFAULT, 'Bàn chải đánh răng', 'cái', 38000),
+    (DEFAULT, 'Dầu gội đầu', 'gói', 5000);
+ROLLBACK;
+
+INSERT INTO "product"("id", "name", "unit", "price")
+VALUES 
+	(DEFAULT, 'Nước rửa chén', 'chai', 32000),
+	(DEFAULT, 'Bột giặt', 'gói', 35000),
+	(DEFAULT, 'Nước xả quần áo', 'gói', 35000),
+	(DEFAULT, 'Xà phòng', 'chai', 50000)
+ROLLBACK;
+COMMIT;
 
 
 -- -----------------------------
 -- Table pack
 -- -----------------------------
--- INSERT INTO "pack" VALUES (DEFAULT, 'Rau - củ - trái cây', 10);  -- Bắp cải, xà lách, nấm, khoai tây, cà rốt, cam, táo
--- INSERT INTO "pack" VALUES (DEFAULT, 'Thịt - cá - hải sản', 10);  -- thịt heo, thịt bò, thịt gà, trứng gà, cá, tôm, mực
--- INSERT INTO "pack" VALUES (DEFAULT, 'Đồ khô', 10)  -- cơm cháy, rong biển, mì gói, xúc xích, 
--- INSERT INTO "pack" VALUES (DEFAULT, 'Đồ uống', 10);  -- nước khoáng, trà xanh, pepsi, nước trái cây, cafe, trà sữa, mật ong
--- INSERT INTO "pack" VALUES (DEFAULT, 'Sữa uống', 10);  -- sữa tươi, sữa chua, sữa bột, sữa lúa mạch, ngũ cốc
--- INSERT INTO "pack" VALUES (DEFAULT, 'Đồ dùng cá nhân', 10);  -- Khẩu trang, nước rửa tay, kem đánh răng, bản chải, dầu gội đầu, sữa tắm
--- INSERT INTO "pack" VALUES (DEFAULT, 'Giặt rửa', 10);  -- nước rửa chén, bột giặt, nước xả quần áo
-
-
+-- INSERT INTO "pack"("id", "name", "limit")
+-- VALUES 
+--     (DEFAULT, 'Rau - củ - trái cây', 10),  -- Bắp cải, xà lách, nấm, khoai tây, cà rốt, cam, táo
+--     (DEFAULT, 'Thịt - cá - hải sản', 10),  -- thịt heo, thịt bò, thịt gà, trứng gà, cá, tôm
+--     (DEFAULT, 'Đồ khô', 10),  -- cơm cháy, rong biển, mì gói, xúc xích, 
+--     (DEFAULT, 'Đồ uống', 10),  -- nước khoáng, nước trái cây, mật ong
+--     (DEFAULT, 'Sữa uống', 10),  -- sữa tươi, sữa chua, sữa lúa mạch, sữa hạt
+--     (DEFAULT, 'Đồ dùng cá nhân', 10),  -- Khẩu trang, nước rửa tay, kem đánh răng, bản chải, dầu gội đầu
+--     (DEFAULT, 'Giặt rửa', 10);  -- nước rửa chén, bột giặt, nước xả quần áo, xà phòng
+	
+-- -----------------------------
+-- Table pack_items
+-- -----------------------------
+-- INSERT INTO "pack_items"("pack_id", "product_id", "quantity")
+-- VALUES
+-- 	('1', )
 
 
 
