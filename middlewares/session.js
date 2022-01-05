2ca42616c108f9179b3b21ce45bb1264687e9f0a
thy0602 <pnmthy0602@gmail.com>
2022-01-06 01:32:48 +0700

    Revert Nhan's branch

--- middlewares/session.js
+++
@@ -1,9 +0,0 @@
-const session = require('express-session');
-
-module.exports = app => {
-    app.use(session({
-        secret: 'radomstring',
-        resave: false,
-        saveUninitialized: true,
-    }));
-}
