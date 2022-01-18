a558483fba3d1658a7688101cd94a44fa8dae636
Nguyễn Tấn Thìn <ngtanthin21@gmail.com>
2022-01-18 10:18:02 +0700

    Finish create, edit, delete location

--- controllers/quarantineLocationsController.js
+++ controllers/quarantineLocationsController.js
@@ -1,8 +1,83 @@
 const express = require("express");
 const router = express.Router();
+const quarantineLocationModel = require('../models/quarantineLocationModel');
 
-router.get('/location', (req, res) => {
-    res.render('location/location');
+// Store new location
+router.post('/new', async (req, res) => {
+    const location = req.body;
+    console.log('get /quarantine-location/new location: ', location);
+
+    try {
+        const result = await quarantineLocationModel.add(location);
+        console.log('get /quarantine-locations/new result: ', result);
+        res.redirect('/quarantine-locations');
+    } catch (error) {
+        console.log('Error post /quarantine-locations/new: ', error);
+        res.status(400).send(error);
+    }
+});
+
+// Update location
+router.post('/:locationId/edit', async (req, res) => {
+    const locationId = req.params.locationId;
+    const location = req.body;
+
+    console.log('post /quarantine-location/:locationId/edit locationId: ', locationId);
+    console.log('post /quarantine-location/:locationId/edit location: ', location);
+
+    try {
+        const result = await quarantineLocationModel.updateByLocationId(locationId, location);
+        console.log('post /quarantine-location/:locationId/edit result: ', result);
+        res.redirect('/quarantine-locations');
+    } catch (error) {
+        console.log('Error post /quarantine-location/:locationId/edit: ', error);
+        res.status(400).send(error);
+    }
+});
+
+// Delete location
+router.post('/:locationId/delete', async (req, res) => {
+    const locationId = req.params.locationId;
+    const data = req.body;
+    console.log('post /quarantine-location/:locationId/delete locationId: ', locationId);
+    console.log('post /quarantine-location/:locationId/delete data: ', data);
+
+    try {
+        const result = quarantineLocationModel.updateByLocationId(locationId, data);
+        res.json(result);
+    } catch (error) {
+        console.log('Error post /quarantine-location/:locationId/delete: ', error);
+        res.status(400).send(error);
+    }
 })
+
+
+// Get a location info by locationId (json)
+router.get('/:locationId', async (req, res) => {
+    const locationId = req.params.locationId;
+    console.log('get /quarantine-locations/:locationId locationId: ', locationId);
+
+    try {
+        const location = await quarantineLocationModel.getByLocationId(locationId);
+        res.json(location);
+    } catch (error) {
+        console.log('Error post /quarantine-locations/:locationId: ', error);
+        res.status(404).send(error);
+    }
+});
+
+// Get list of all locations
+router.get('/', async (req, res) => {
+    try {
+        const locationList = await quarantineLocationModel.getAll();
+        res.render('location/location', {
+            locationList
+        });
+    } catch (error) {
+        console.log('Error get /quarantine-locations: ', error);
+        res.status(404).send(error);
+    }
+    
+});
 
 module.exports = router;
---
+++ models/quarantineLocationModel.js
@@ -0,0 +1,64 @@
+const schema = 'public';
+const pgp = require('pg-promise')({
+    capSQL: true,
+});
+const { db } = require('../db/db_config.js');
+
+const tableName = "quarantine_location";
+const tableFields = {
+    id: 'id',   // Primary Key
+    name: 'name',
+    capacity: 'capacity',
+    occupancy: 'occupancy',
+    is_deleted: 'is_deleted'
+}
+
+exports.getAll = async () => {
+    const table = new pgp.helpers.TableName({ table: tableName, schema: schema });
+    const queryStr = pgp.as.format(`SELECT * FROM $1 WHERE "${tableFields.is_deleted}"='False' ORDER BY ${tableFields.id} ASC`, table);
+    try {
+        const res = await db.any(queryStr);
+        return res;
+    } catch (e) {
+        console.log("Error quarantineLocationModel/getAll: ", e);
+        throw e;
+    }
+}
+
+exports.getByLocationId = async (locationId) => {
+    const table = new pgp.helpers.TableName({ table: tableName, schema: schema });
+    const queryStr = pgp.as.format(`SELECT * FROM $1 WHERE "${tableFields.id}"='${locationId}';`, table);
+    try {
+        const res = await db.one(queryStr);
+        return res;
+    } catch (error) {
+        console.log("Error quarantineLocationModel/getByLocationId: ", e);
+        throw e;
+    }
+}
+
+exports.updateByLocationId = async (locationId, entity) => {
+    const table = new pgp.helpers.TableName({ table: tableName, schema: schema });
+    const condition = pgp.as.format(` WHERE "${tableFields.id}"='${locationId}'`);
+    const queryStr = pgp.helpers.update(entity, Object.keys(entity), table) + condition + ' RETURNING *';
+
+    try {
+        const res = await db.one(queryStr);
+        return res;
+    } catch (error) {
+        console.log("Error packModel/updateByLocationId: ", error);
+        throw error;
+    }
+}
+
+exports.add = async (entity) => {
+    const table = new pgp.helpers.TableName({table: tableName, schema: schema});
+    const queryStr = pgp.helpers.insert(entity, null, table) + ' RETURNING *';
+    try {
+        const res = await db.one(queryStr);
+        return res;
+    } catch (error) {
+        console.log('Error quarantineLocationModel/add: ', error);
+        throw error;
+    }
+}
